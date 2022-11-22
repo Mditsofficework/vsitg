4f008e649ac234f70ae9d1b52e2fa590f38f9661
MDITS <mditsoftwareszone@gmail.com>
2022-11-22 16:08:19 +0530

    first-commit

---
+++ src/main/java/com/example1/jenkinwithspringboot/JenkinwithspringbootApplication.java
@@ -0,0 +1,24 @@
+package com.example1.jenkinwithspringboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinwithspringbootApplication implements CommandLineRunner {
+	public static final Logger logger = LoggerFactory.getLogger(JenkinwithspringbootApplication.class);
+
+	public static void main(String[] args) {
+		logger.info("it's a CI job");
+		SpringApplication.run(JenkinwithspringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("my second CI");
+		
+	}
+
+}
---
+++ src/test/java/com/example1/jenkinwithspringboot/JenkinwithspringbootApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example1.jenkinwithspringboot;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinwithspringbootApplicationTests {
+	public static final org.slf4j.Logger logger = LoggerFactory.getLogger(JenkinwithspringbootApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+		logger.info("it's my test class");
+		assertEquals(true,true);
+	}
+
+}
